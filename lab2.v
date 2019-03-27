7a787da1fd6a1bb41ee65d5371ae89863c11aaa7
ltsaur <48870997+ltsaur@users.noreply.github.com>
2019-03-26 20:55:50 -0700

    Update lab2.v
    convert tab to space

--- lab2.v
+++ lab2.v
@@ -64,42 +64,42 @@ assign i_data_rdy_local_posedge = i_data_rdy_tap[0] & ~i_data_rdy_tap[1];
 // combine strobs generated within 2 cycles of i_clk_in
 always @(posedge i_clk_in) begin
     if(i_rst) begin
-	    i_data_rdy_tap[1:0] <= 2'b00;
+        i_data_rdy_tap[1:0] <= 2'b00;
         x <= 2'h00;
-	    y <= 2'h00;
-	    l_cin <= 1'b0;
-		rst_test <= 1;
-	end
-	else begin
-		i_data_rdy_tap[1:0] <= {i_data_rdy_tap[0], i_data_rdy};
+        y <= 2'h00;
+        l_cin <= 1'b0;
+        rst_test <= 1;
+    end
+    else begin
+        i_data_rdy_tap[1:0] <= {i_data_rdy_tap[0], i_data_rdy};
 
-        if(i_data_rdy_local_posedge) begin		
-	        rst_test <= 0;
+        if(i_data_rdy_local_posedge) begin      
+            rst_test <= 0;
             x[7:0] <= i_r1[7:0];
-	        if (i_substrate_signal) begin
-		        y[7:0] <= {~i_r2[7], ~i_r2[6], ~i_r2[4], ~i_r2[4], ~i_r2[3], ~i_r2[2], ~i_r2[1], ~i_r2[0]};
-		        l_cin <= 1'b1;
-	        end
-	        else begin
-	            y[7:0] <= i_r2[7:0];
-		        l_cin <= 1'b0;
-			end
-		end
-		else begin
+            if (i_substrate_signal) begin
+                y[7:0] <= {~i_r2[7], ~i_r2[6], ~i_r2[4], ~i_r2[4], ~i_r2[3], ~i_r2[2], ~i_r2[1], ~i_r2[0]};
+                l_cin <= 1'b1;
+            end
+            else begin
+                y[7:0] <= i_r2[7:0];
+                l_cin <= 1'b0;
+            end
+        end
+        else begin
             x <= x;
-	        y <= y;
-	        l_cin <= l_cin;
-		    rst_test <= rst_test;
-		end
-	end
+            y <= y;
+            l_cin <= l_cin;
+            rst_test <= rst_test;
+        end
+    end
 end
 
 fourbit_adder adder_UT(.sum (result[3:0]), 
-			           .carry (result[4]), 
-			           .r1 (x[3:0]), 
-					   .r2 (y[3:0]), 
-					   .ci (l_cin)
-					   );
+                       .carry (result[4]), 
+                       .r1 (x[3:0]), 
+                       .r2 (y[3:0]), 
+                       .ci (l_cin)
+                       );
 
 
 //------------------------------------------
@@ -119,22 +119,22 @@ assign adder_data_ready_wire = i_data_rdy_local_posedge;
 assign o_adder_data_ready = adder_data_ready_reg[13] & ~adder_data_ready_reg[15];   
 assign o_rdy = o_adder_data_ready;
 
-always @(posedge i_clk_in or posedge i_rst) begin		
+always @(posedge i_clk_in or posedge i_rst) begin       
     if(i_rst) begin
         adder_data_ready_reg <= 4'h0000;
-	end	
-	else begin
-		adder_data_ready_reg[15:0] <= {adder_data_ready_reg[14:0], adder_data_ready_wire}; 
-		
+    end 
+    else begin
+        adder_data_ready_reg[15:0] <= {adder_data_ready_reg[14:0], adder_data_ready_wire}; 
+        
         if(adder_data_ready_reg[11]) begin
             if(i_substrate_signal)
-                result_reg[7:0] <= {1'b0, 1'b0, 1'b0, 0, result[3:0]}; //latch in result	
-	        else
+                result_reg[7:0] <= {1'b0, 1'b0, 1'b0, 0, result[3:0]}; //latch in result    
+            else
                 result_reg[7:0] <= {1'b0, 1'b0, 1'b0, result[4:0]}; //latch in result
-	    end
-	    else
-	        result_reg <= result_reg;
-	end
+        end
+        else
+            result_reg <= result_reg;
+    end
 end
 
 assign o_sum[3:0] = result_reg[3:0];
@@ -173,10 +173,10 @@ assign debug_z_test[7:0] = {is_uart_rx_b7_0 , is_uart_rx_b6_1 , is_uart_rx_b5_1,
 wire [7:0] debug_LB_test; //{ 0x7B
 assign debug_LB_test[7:0] = {is_uart_rx_b7_0 , is_uart_rx_b6_1 , is_uart_rx_b5_1, is_uart_rx_b4_1,
                               is_uart_rx_b3_1 , is_uart_rx_b2_0, is_uart_rx_b1_1, is_uart_rx_b0_1};
-							  
+                              
 wire [7:0] debug_OR_test; //| 0x7C
 assign debug_OR_test[7:0] = {is_uart_rx_b7_0 , is_uart_rx_b6_1 , is_uart_rx_b5_1, is_uart_rx_b4_1,
-                              is_uart_rx_b3_1 , is_uart_rx_b2_1, is_uart_rx_b1_0, is_uart_rx_b0_0};							  
+                              is_uart_rx_b3_1 , is_uart_rx_b2_1, is_uart_rx_b1_0, is_uart_rx_b0_0};                           
 wire [7:0] debug_RB_test; //} 0x7D
 assign debug_RB_test[7:0] = {is_uart_rx_b7_0 , is_uart_rx_b6_1 , is_uart_rx_b5_1, is_uart_rx_b4_1,
                               is_uart_rx_b3_1 , is_uart_rx_b2_1, is_uart_rx_b1_0, is_uart_rx_b0_1};
@@ -207,51 +207,51 @@ always @ (posedge i_clk_in)
 begin
 
     if(i_rst) begin
-	    debug_reg[7:0] <= {1'b0,1'b0,1'b0,
-		                  rst_test, 
-						  debug_adder_data_rdy_delay_line, 
-		                  debug_adder_rdy_self_clear, 
-						  debug_y, 
-						  debug_x};
-	end else
+        debug_reg[7:0] <= {1'b0,1'b0,1'b0,
+                          rst_test, 
+                          debug_adder_data_rdy_delay_line, 
+                          debug_adder_rdy_self_clear, 
+                          debug_y, 
+                          debug_x};
+    end else
     if(rst_test) begin
-	    debug_reg[7:0] <= {1'b0,1'b0,1'b0, 
-						   debug_adder_rdy_self_clear, 
-						   debug_adder_rdy_self_clear, 
-						   debug_cin,
-						   debug_y, 
-						   debug_x};
-	end else
+        debug_reg[7:0] <= {1'b0,1'b0,1'b0, 
+                           debug_adder_rdy_self_clear, 
+                           debug_adder_rdy_self_clear, 
+                           debug_cin,
+                           debug_y, 
+                           debug_x};
+    end else
 
     if (i_ctrl_signal) begin
         if(debug_is_DEL) begin  //some debug reset 
-	        debug_reg[7:0] <= {2'h00};
-	    end else 
-		begin
-		    if(debug_is_CR) begin
-	            debug_reg[7:0] <= {1'b0,1'b0,1'b0, result[4:0]}; //local results		   
-	        end else
-		    if(debug_is_z) begin
-	            debug_reg[7:0] <= {1'b0,1'b0,1'b0, l_cin, rst_test, result[7:5]}; //local results		   
-	        end else
-		    if(debug_is_OR) begin
-	            debug_reg[7:0] <= {i_r1[7:0]}; //local results		   
-	        end else
-		    if(debug_is_NOT) begin
-	            debug_reg[7:0] <= {i_r2[7:0]}; //local results		   
-	        end  else 
-		    if(debug_is_LB) begin
-	            debug_reg[7:0] <= {x[7:0]}; //{
-            end else 		
-		    if(debug_is_RB) begin
-	            debug_reg[7:0] <= {y[7:0]}; //}
+            debug_reg[7:0] <= {2'h00};
+        end else 
+        begin
+            if(debug_is_CR) begin
+                debug_reg[7:0] <= {1'b0,1'b0,1'b0, result[4:0]}; //local results           
+            end else
+            if(debug_is_z) begin
+                debug_reg[7:0] <= {1'b0,1'b0,1'b0, l_cin, rst_test, result[7:5]}; //local results          
+            end else
+            if(debug_is_OR) begin
+                debug_reg[7:0] <= {i_r1[7:0]}; //local results         
+            end else
+            if(debug_is_NOT) begin
+                debug_reg[7:0] <= {i_r2[7:0]}; //local results         
+            end  else 
+            if(debug_is_LB) begin
+                debug_reg[7:0] <= {x[7:0]}; //{
+            end else        
+            if(debug_is_RB) begin
+                debug_reg[7:0] <= {y[7:0]}; //}
             end 
-		end
-	end 
-	
-	else begin
-	    debug_reg[7:0] <= {1'b0,1'b0,1'b0, result_reg[4:0]};  //results drive to interface
-	end
+        end
+    end 
+    
+    else begin
+        debug_reg[7:0] <= {1'b0,1'b0,1'b0, result_reg[4:0]};  //results drive to interface
+    end
 end
   
 assign o_debug_led[7:0] = debug_reg[7:0];
